ab83ad8033e6935b6751c3abe1630674f9f3573f
Anmol Sharma <anmolsharma0234@gmail.com>
2025-09-29 00:26:09 +0530

    feat: add hooks for managing user, scores and cohorts

---
+++ src/hooks/cohortHooks.ts
@@ -0,0 +1,90 @@
+import apiService from '../services/apiService.ts';
+import { createUseMutation, createUseQuery } from '../http';
+import {
+  PaginatedDataDto,
+  PaginatedQueryDto,
+  GetCohortResponseDto,
+  CreateCohortRequestDto,
+  UpdateCohortRequestDto,
+  UpdateCohortWeekRequestDto,
+  JoinWaitlistRequestDto,
+  UserCohortWaitlistResponseDto,
+} from '../types/api.ts';
+
+// ===============
+// Queries
+// ===============
+
+export const useCohorts = createUseQuery<
+  PaginatedDataDto<GetCohortResponseDto>,
+  PaginatedQueryDto
+>(
+  (query) => ['cohorts', query],
+  (query) => () => apiService.listCohorts(query),
+);
+
+export const useMyCohorts = createUseQuery<
+  PaginatedDataDto<GetCohortResponseDto>,
+  PaginatedQueryDto
+>(
+  (query) => ['cohorts', 'me', query],
+  (query) => () => apiService.listMyCohorts(query),
+);
+
+export const useCohort = createUseQuery<GetCohortResponseDto, string>(
+  (cohortId) => ['cohort', cohortId],
+  (cohortId) => () => apiService.getCohort(cohortId),
+);
+
+export const useMyWaitlistStatus = createUseQuery<
+  UserCohortWaitlistResponseDto,
+  void
+>(
+  () => ['cohorts', 'waitlist', 'me'],
+  () => apiService.getUserWaitlistStatus,
+);
+
+// ===============
+// Mutations
+// ===============
+
+export const useCreateCohort = createUseMutation<void, CreateCohortRequestDto>(
+  apiService.createCohort,
+);
+
+export const useUpdateCohort = createUseMutation<
+  void,
+  { cohortId: string; body: UpdateCohortRequestDto }
+>(
+  ({ cohortId, body }) => apiService.updateCohort(cohortId, body),
+  {
+    queryInvalidation: async ({ variables: { cohortId } }) => {
+      await useCohort.invalidate(cohortId);
+    },
+  },
+);
+
+export const useUpdateCohortWeek = createUseMutation<
+  void,
+  { cohortId: string; cohortWeekId: string; body: UpdateCohortWeekRequestDto }
+>(
+  ({ cohortWeekId, body }) => apiService.updateCohortWeek(cohortWeekId, body),
+  {
+    queryInvalidation: async ({ variables: { cohortId } }) => {
+      await useCohort.invalidate(cohortId);
+    },
+  },
+);
+
+export const useJoinCohort = createUseMutation<void, { cohortId: string }>(
+  ({ cohortId }) => apiService.joinCohort(cohortId),
+);
+
+export const useJoinCohortWaitlist = createUseMutation<
+  void,
+  JoinWaitlistRequestDto
+>(apiService.joinCohortWaitlist, {
+  queryInvalidation: async () => {
+    await useMyWaitlistStatus.invalidate();
+  },
+});
---
+++ src/hooks/scoreHooks.ts
@@ -0,0 +1,58 @@
+import apiService from '../services/apiService.ts';
+import { createUseMutation, createUseQuery } from '../http';
+import {
+  GetUsersScoresResponseDto,
+  ListScoresForCohortAndWeekResponseDto,
+  UpdateScoresRequestDto,
+} from '../types/api.ts';
+
+// ===============
+// Queries
+// ===============
+
+export const useScoresForCohortAndWeek = createUseQuery<
+  ListScoresForCohortAndWeekResponseDto,
+  { cohortId: string; weekId: string }
+>(
+  ({ cohortId, weekId }) => ['scores', 'cohort', cohortId, 'week', weekId],
+  ({ cohortId, weekId }) => () =>
+    apiService.listScoresForCohortAndWeek(cohortId, weekId)
+);
+
+export const useMyScores = createUseQuery<GetUsersScoresResponseDto, void>(
+  () => ['scores', 'me'],
+  () => apiService.getMyScores
+);
+
+export const useUserScores = createUseQuery<
+  GetUsersScoresResponseDto,
+  string
+>(
+  (userId) => ['scores', 'user', userId],
+  (userId) => () => apiService.getUserScores(userId)
+);
+
+// ===============
+// Mutations
+// ===============
+
+export const useUpdateScoresForUserCohortAndWeek = createUseMutation<
+  void,
+  { userId: string; cohortId: string; weekId: string; body: UpdateScoresRequestDto }
+>(
+  ({ userId, cohortId, weekId, body }) =>
+    apiService.updateScoresForUserCohortAndWeek(userId, cohortId, weekId, body),
+  {
+    queryInvalidation: async ({variables: {userId, cohortId, weekId}}) => {
+      await useScoresForCohortAndWeek.invalidate({cohortId, weekId});
+      await useUserScores.invalidate(userId);
+    },
+  }
+);
+
+export const useAssignGroupsForCohortWeek = createUseMutation<
+  void,
+  { weekId: string }
+>(
+  ({ weekId }) => apiService.assignGroupsForCohortWeek(weekId),
+);
