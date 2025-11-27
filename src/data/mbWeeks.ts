e4000cae790d9ea3b40d55139617ac43dd311e5b
Vayras <Vayras>
2025-11-28 00:28:54 +0530

    mb questions update

--- src/data/mbWeeks.ts
+++ src/data/mbWeeks.ts
@@ -7,21 +7,18 @@ export const mbWeeks: WeekContent[] = [
     content: "Chapter 1: Introduction - Chapter 2: How Bitcoin Works",
     gdQuestions: [
       "What were the major problems of Digital Currencies before Bitcoin? How did Bitcoin solve them?",
-      "What are the major components of the Bitcoin System? What does it mean by \"consensus rules\"?",
-      "What are the different types of Bitcoin Wallets? Which one among them is most and least secured?",
-      "What are the different types of Bitcoin Nodes? What are the tradeoffs between them? When is it applicable to use a \"light node\"?",
       "Do you think it would be better if bitcoin transactions were reversible? What are the potential benefits and drawbacks?",
-      "Do you believe Bitcoin needs to compete with Visa/MasterCard to succeed? Why or why not?",
       "What is the Bitcoin Issuance Rate? How do we know the supply cap of Bitcoin is 21 million?",
       "Is Bitcoin private? Does Bitcoin need to be private? Why isn't Bitcoin fungible?",
-      "What is transaction fee? How is it calculated? Whats the effect of transaction fee on transaction confirmation?",
-      "What are the two main components of a Transaction? What does each component include?",
-      "What are change addresses? When are change addresses required? What happens if the user (or wallet sofwtware) forgets to include the change address in a transaction? When will we not require a change address?",
-      "What does coinselection mean? Can you name few popular coinselection algorithms? Is it better to manually select coins or let the wallet software automatically handle it?",
-      "How is Proof of Work mining similar to \"needle in haystack\" problem? What happens when two blocks are mined by different miners at the same height, how does the network decide which is the \"right\" block?",
+      "What is a transaction fee? How is it calculated? What's the effect of transaction fee on transaction confirmation?",
+      "What are change addresses? When are change addresses required? What happens if the user (or wallet software) forgets to include the change address in a transaction? When will we not require a change address?",
       "What does it mean by a transaction to be \"confirmed\"? How many confirmation is usually acceptable? Is it okay to accept unconfirmed transactions? Why or why not?",
-      "What is the lowest denomination of Bitcoin? Can we run out of decimals to denominate the smallest unit, as Bitcoin adoption increases?",
       "Why did Satoshi said transactions are \"chain of digital signatures\"? What is a UTXO? Whats the difference between UTXO and Account based model? Why did Bitcoin choose to stick with UTXO model?"
+    ],
+    bonusQuestions: [
+      "Do you believe Bitcoin needs to compete with Visa/MasterCard to succeed? Why or why not?",
+      "What are the two main components of a Transaction? What does each component include?",
+      "What is the lowest denomination of Bitcoin? Can we run out of decimals to denominate the smallest unit, as Bitcoin adoption increases?"
     ]
   },
   {
@@ -31,25 +28,19 @@ export const mbWeeks: WeekContent[] = [
     gdQuestions: [
       "Why is Bitcoin Core called a \"Reference Implementation\"? What other implementations of Bitcoin are out there? Is it preferable to have many implementations of Bitcoin?",
       "What are BIPs (Bitcoin Improvement Proposals)? What are their role in the Bitcoin development process?",
-      "What are the few major configuration options in Bitcoin Core? As a home node operator, which one do you think is most useful?",
-      "What is `txindex`? When would a node operator use this configuration flag? What are the overheads of turning this configuration on?",
-      "What is `bitcoin-cli`? What is it used for? Have you explored any `bitcoin-cli` commands? Which commands do you find most useful as a home node operator?",
-      "What is the approximate disk space requirement for running Bitcoin Core? What are the approximate network bandwidth and memory requirements? Do you think it's \"cheap\" to run a node?",
-      "What are commands you would use sequentially to get the fully deserialized coinbase transaction of the Block 100, using `bitcoin-cli`?",
-      "How can you verify the signatures of download binaries of Bitcoin Core? What are the potential damages of running a malicious version of the software?",
+      "What is bitcoin-cli? What is it used for? Have you explored any bitcoin-cli commands? Which commands do you find most useful as a home node operator?",
       "Describe the functions of 'private keys', 'public keys', 'Bitcoin addresses', and 'digital signatures'. What is the mathematical relationship between them and how are they used in Bitcoin?",
       "What is the discrete logarithm problem, and why is it considered hard to solve?",
       "What is asymmetry in cryptography, and why are one-way functions useful? Identify a one-way cryptographic function used in deriving public keys from private keys.",
       "Describe instances where private keys have been compromised due to lack of entropy. Why is it important to use a cryptographically secure pseudorandom number generator to produce a private key? What are some techniques of generating pseudo random numbers?",
-      "What is secp256k1, and what are the curve constants used in Bitcoin? How is ECDSA malleable?",
-      "What is a Bitcoin address, and why are addresses used instead of raw public keys?",
-      "What hash functions are used in the generation of Bitcoin addresses?",
-      "Why is Base58 used for Bitcoin addresses instead of Base64? Would you make any modifications beyond Base58?",
-      "What are the different private key formats used by wallets?",
-      "What are compressed public keys, and what are the benefits of compressing a public key?",
       "What are the different kinds of Bitcoin addresses? Explain the differences between P2PKH and P2SH.",
       "What is the difference between a hot wallet and cold storage?",
       "What is bech32? How is it better than base58? Are there any problems in bech32? How are they solved?"
+    ],
+    bonusQuestions: [
+      "What is the approximate disk space requirement for running Bitcoin Core? What are the approximate network bandwidth and memory requirements? Do you think it's \"cheap\" to run a node?",
+      "How can you verify the signatures of download binaries of Bitcoin Core? What are the potential damages of running a malicious version of the software?",
+      "What hash functions are used in the generation of Bitcoin addresses?"
     ]
   },
   {
